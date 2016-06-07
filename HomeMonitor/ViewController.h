63576f8e21afbdae7dfbe54244fbf1c196f447fe
drphilippou <paul@MacBook-Air.local>
2016-06-07 13:02:08 -1000

    Initial Commit

---
+++ HomeMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeMonitor
+//
+//  Created by Paul Philippou on 6/7/16.
+//  Copyright (c) 2016 IntelligentOhanaSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HomeMonitor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HomeMonitor
+//
+//  Created by Paul Philippou on 6/7/16.
+//  Copyright (c) 2016 IntelligentOhanaSolutions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
