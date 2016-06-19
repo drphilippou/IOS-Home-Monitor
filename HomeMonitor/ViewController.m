e162e48e50a0b91965148448b9443c9c3a769da9
drphilippou <dr.paul.philippou@gmail.com>
2016-06-18 18:36:07 -1000

    downloading a sample file from the website, converting the file back to a dictionary

--- HomeMonitor/ViewController.m
+++ HomeMonitor/ViewController.m
@@ -11,7 +11,12 @@
 @interface ViewController ()
 {
  
+
     NSTimer* accessWebsiteTimer;
+    NSURLConnection* webConnection;
+    NSMutableData* webData;
+    long lastRxSec;
+
 }
 @property (strong,nonatomic) NSMutableDictionary* data;
 
@@ -24,27 +29,136 @@
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     //test the version control
     
+    //init variables
+    lastRxSec = 0;
+    
+    
     //start the timers
     if (accessWebsiteTimer==nil) {
-        accessWebsiteTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(accessWebsite) userInfo:nil repeats:YES];
+        accessWebsiteTimer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(accessWebsite) userInfo:nil repeats:YES];
     }
     
+    //download the first instance
+    [self accessWebsite];
     
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
 }
 
 -(void)accessWebsite
 {
     NSLog(@"accessing the website");
+    
+//    if (webConnection != nil) {
+//        NSTimeInterval now = [self currentTimeSec];
+//        NSTimeInterval elapsed = now - updateWebTime;
+//        NSLog(@"OBRDS Update Vehicle Start... UPdate in progress %f",elapsed);
+//        
+//        if (elapsed>30) {
+//            NSLog(@"Update Vehicle timed out restarting");
+//            vehicleConnection = nil;
+//            vehicleData = nil;
+//        }
+//        return;
+//    }
+
+    //determine how much history we need
+    
+    
+    
+    //set the URL
+    //NSURL *url = [NSURL URLWithString:@"http://OBRuser:OBRUserPassword@ios-hawaii.com/OBRroot/allvehicles.json"];
+    NSURL *url = [NSURL URLWithString:@"http://ios-hawaii.org/20165.json"];
+    
+    //set the start time
+    //updateWebTime = [self currentTimeSec];
+    
+    //start the asynchronous transfer
+    webData = [[NSMutableData alloc] init];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url
+                                         cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
+                                     timeoutInterval:120];
+    webConnection = [[NSURLConnection alloc] initWithRequest:req
+                                                        delegate:self
+                                                startImmediately:YES];
+    
+    
 }
+
+
+#pragma mark - Access Data Connection
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    
+    NSLog(@"received response");
+    
+    //get the etag
+    NSString* eTag = nil;
+    NSDictionary* d;
+    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse*)response;
+    if ([response respondsToSelector:@selector(allHeaderFields)]) {
+        d = [httpResponse allHeaderFields];
+        eTag = d[@"Etag"];
+    }
+    
+//    if (eTag != nil && [eTag isEqualToString:lastVehicleEtag]) {
+//        [connection cancel];
+//        vehicleConnection = nil;
+//        vehicleData = nil;
+//        NSLog(@"OBRDS Server sending stale file... aborting connection");
+//    } else {
+//        lastVehicleEtag = [eTag copy];
+//    }
+}
+
+- (void)connection:(NSURLConnection *)conn didReceiveData:(NSData *)d {
+    
+    [webData appendData:d];
+    NSLog(@"received data len:%lu", (unsigned long)webData.length);
+}
+
+
+- (void)connection:(NSURLConnection *)conn didFailWithError:(NSError *)error {
+    
+    NSLog(@"failed with error");
+    webConnection = nil;
+    webData = nil;
+}
+
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)conn {
+    NSLog(@"did finish loading");
+    webConnection = nil;
+    //[self updateVehiclesEnd:vehicleData];
+    
+    NSDictionary *rxJSON = [NSJSONSerialization JSONObjectWithData:webData
+                                                                 options:0
+                                                                   error:nil];
+    for (NSString* k in rxJSON) {
+        NSLog(k);
+        NSDictionary* d = rxJSON[k];
+        NSLog(d.description);
+        long secs = [[d objectForKey:@"secs"] longLongValue];
+        if (secs >lastRxSec) {
+            lastRxSec = secs;
+        }
+    }
+ 
+    
+    
+    webData = nil;
+}
+
+
+
 
 @end
