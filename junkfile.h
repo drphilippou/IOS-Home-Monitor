335e8ef28140b5de78ed4f843ce5ad29819a69d6
drphilippou <dr.paul.philippou@gmail.com>
2016-06-24 12:22:19 -1000

    files should only exist in the branch

---
+++ junkfile.h
@@ -0,0 +1,13 @@
+//
+//  junkfile.h
+//  HomeMonitor
+//
+//  Created by Paul Philippou on 6/24/16.
+//  Copyright (c) 2016 IntelligentOhanaSolutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface junkfile : NSObject
+
+@end
